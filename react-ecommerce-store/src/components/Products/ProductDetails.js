b411e370428f7cf070be883ad43f86e858cd9b60
Arpit Trivedi <arpit1trivedi@gmail.com>
2020-04-30 10:09:45 +0530

    Added Product details info

--- react-ecommerce-store/src/components/Products/ProductDetails.js
+++ react-ecommerce-store/src/components/Products/ProductDetails.js
@@ -4,23 +4,35 @@ import { ProductContext } from '../../GlobalContext';
 const ProductDetails = () => {
     const context = useContext(ProductContext);
     const { detailProduct } = context;
-    const {id, img, title, price, company, info, inCart} = detailProduct;
+    const { id, img, title, price, company, info, inCart } = detailProduct;
     console.log(detailProduct);
-    return(
+    return (
         <>
             <div className="container py-5">
                 {/* Product Title */}
                 <div className="row">
                     <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
-                        <h1>{ title }</h1>
+                        <h1>{title}</h1>
                     </div>
                 </div>
                 {/* Product Info */}
                 <div className="row">
                     <div className="col-10 mx-auto col-md-6 my-3">
-                        <img src={ img } alt="Product" className="img-fluid" />
+                        <img src={img} alt="Product" className="img-fluid" />
                     </div>
-                    <div className="col-10 mx-auto col-md-6 my-3 text-capitalize"></div>
+                    <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                        <h2>model : {title}</h2>
+                        <h4 className="text-uppercase text-muted mt-3 mb-2">
+                            made by : <span className="text-title text-uppercase">{company}</span>
+                        </h4>
+                        <h4 className="text-blue">
+                            <strong>price : <span>$ {price}</span></strong>
+                        </h4>
+                        <p className="text-capitalize font-weight-bold mt-3 mb-0">
+                            some info about product:
+                        </p>
+                        <p className="text-muted lead">{info}</p>
+                    </div>
                 </div>
             </div>
         </>
